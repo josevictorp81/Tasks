6920bc8617ff6bcef9cc7d777de2fc08ebc8c171
josevictorp81 <josevictorp81@gmail.com>
2021-03-30 11:55:11 -0300

    create task

--- tasks/core/urls.py
+++ tasks/core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.taskList, name='taskList'),
     path('task/<int:id>/', views.taskDetail, name='taskDetail'),
+    path('newTask/', views.newTask, name='newTask'),
 ]
--- tasks/core/views.py
+++ tasks/core/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Task
+from .forms import TaskModelForm
 
 def taskList(request):
-    task = Task.objects.all()
+    task = Task.objects.all().order_by('-created')
 
     data = {
         'task': task,
@@ -16,4 +17,19 @@ def taskDetail(request, id):
     data = {
         'task': task,
     }
-    return render(request, 'task/detail.html', data)  
+    return render(request, 'task/detail.html', data)
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'Doing'
+            task.save()
+            return redirect('taskList')
+    else:
+        form = TaskModelForm()
+        data = {
+            'form': form,
+        }
+        return render(request, 'task/newTask.html', data)
